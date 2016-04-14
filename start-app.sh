87f3a77678c93921a2b1ba6cbdbf11cff7efd08e
Paul Popoff <oc14@ya.ru>
2016-04-15 02:39:24 +0300

    compilation phase was added

--- start-app.sh
+++ start-app.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# clean compile before run
+rebar clean compile
+
 # -s runs function: -s $MODULE $RUN
 # -pa CLASSPATH
 erl -pa ebin deps/ranch/ebin deps/goldrush/ebin deps/lager/ebin -s wall run
