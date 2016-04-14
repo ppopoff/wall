77e95ec7b72bbc6f27e47ed40d62021e736876f3
Paul Popoff <oc14@ya.ru>
2016-04-14 04:02:14 +0300

    using lager instead of LOG macro, ranch server is created here, error logging was also added

--- src/wall.erl
+++ src/wall.erl
@@ -6,28 +6,30 @@
 -export([start/2, shutdown/0, stop/1]).
 
 
--define(LOG, lager).
-
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
-start(_StartType, StartArgs) ->
-    ?LOG:info("Starting the ranch listener"),
+start(_StartType, _StartArgs) ->
+    lager:info("Starting the ranch listener"),
 
-    %% TODO: eliminate port hard-coding (use erlang configs instead)
-    ranch:start_listener(
-        wall, 1, ranch_tcp, [{port, 1337}], wall_protocol, []),
+    NumberOfAcceptors = 1,
+    Port = 8000,
+    Protocol = wall_protocol,
 
-    ?LOG:info("Starting the application supervisor"),
-    case wall_sup:start_link(StartArgs) of
+    %% Pay attention to [{active, once}]
+    {ok, _} = ranch:start_listener(
+        wall_tcp, NumberOfAcceptors, ranch_tcp, [{port, Port}], Protocol, []),
+
+    case wall_sup:start_link() of
         {ok, Pid} -> {ok, Pid};
-        Error     -> Error
+        Error     -> lager:error("Unable to start supervisor"),
+                     Error
     end.
 
 stop(_State) ->
-    ?LOG:info("Shutting down the application~n"),
+    lager:info("Shutting down the application~n"),
     ok.
 
 shutdown() ->
