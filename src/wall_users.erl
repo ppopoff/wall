a6f7682c4b98cdb74d7071425f1ff7d80a1ef08c
ppopov <oc14@ya.ru>
2016-04-14 18:37:00 +0300

    storage was implemented, and renamed

---
+++ src/wall_users.erl
@@ -0,0 +1,118 @@
+-module(wall_users).
+-behaviour(gen_server).
+
+-define(SERVER, ?MODULE).
+
+%% ------------------------------------------------------------------
+%% API Function Exports
+%% ------------------------------------------------------------------
+
+-export([start_link/0]).
+-export([stop/0]).
+-export([reg/1]).
+-export([exist/1]).
+-export([find/1]).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Exports
+%% ------------------------------------------------------------------
+
+-export([init/1]).
+-export([handle_call/3]).
+-export([handle_cast/2]).
+-export([handle_info/2]).
+-export([terminate/2]).
+-export([code_change/3]).
+
+
+%% ------------------------------------------------------------------
+%% API Function Definitions
+%% ------------------------------------------------------------------
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+
+stop() ->
+    gen_server:cast(?SERVER, stop).
+
+
+reg(UserName) when is_atom(UserName) ->
+    gen_server:call(?SERVER, {reguser, UserName}).
+
+-spec exist(atom()) -> boolean().
+exist(UserName) when is_atom(UserName) ->
+    gen_server:call(?SERVER, {'is registered', UserName}).
+
+
+-spec find(atom()) -> [{atom(), integer()}].
+find(UserName) ->
+    gen_server:call(?SERVER, {find, UserName}).
+
+
+%% ------------------------------------------------------------------
+%% gen_server Function Definitions
+%% ------------------------------------------------------------------
+
+init([]) ->
+    io:format("Creating a table"),
+    TableId = ets:new(storage, [private, set]),
+    io:format("this is my table_id: ~tp", [TableId]),
+    {ok, TableId}.
+
+terminate(Reason, TableId) ->
+    io:format("Stopping the table service~n"),
+    ets:delete(TableId),
+    io:format("Terminated by following reason: ~tp~n", [Reason]),
+    ok.
+
+
+%% This feature is not supported
+handle_info(_Info, TableId) ->
+    {noreply, TableId}.
+
+
+%% This feature is not supported
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+
+handle_call({'is registered', UserName}, _From, TableId) ->
+   {reply, is_registered(ets:lookup(TableId, UserName)), TableId};
+handle_call({reguser, UserName}, _From, TableId) ->
+   {reply, register_user(TableId, UserName), TableId};
+handle_call({find, UserName}, _From, TableId) ->
+    {reply, find_user(TableId, UserName), TableId}.
+
+
+%% Stops the server
+handle_cast(stop, TableId) ->
+    {stop, normal, TableId}.
+
+
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------
+
+% Defines whether user is registred by
+% results of lookup function
+is_registered([])    -> false;
+is_registered([_])   -> true.
+
+% Registers user with given name 
+register_user(TableId, UserName) when is_atom(UserName) ->
+    RegistrationDate = os:timestamp(),
+    Status = ets:insert(TableId, {UserName, get_time_millis(RegistrationDate)}),
+    io:format("User ~tp registered at: ", [Status]),
+    io:format("~tp ~n", [RegistrationDate]),
+    {ok, UserName, RegistrationDate}.
+ 
+
+% Returns name and registration date
+find_user(TableId, UserName) when is_atom(UserName) ->
+    ets:lookup(TableId, UserName).
+
+% Returns current time (since epoch) in milliseconds
+get_time_millis(Now) ->
+    {Mega, Sec, Micro} = Now,
+    (Mega * 1000000 + Sec) * 1000000 + Micro.
