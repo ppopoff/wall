edfe39590507840c801e39c186d438b121772b9d
Paul Popoff <oc14@ya.ru>
2016-04-16 02:17:08 +0300

    Echo server functionality restored

--- src/wall_protocol.erl
+++ src/wall_protocol.erl
@@ -14,53 +14,64 @@
 %% ------------------------------------------------------------------
 
 -export([init/1]).
+-export([init/4]).
 -export([handle_call/3]).
 -export([handle_cast/2]).
 -export([handle_info/2]).
 -export([terminate/2]).
 -export([code_change/3]).
 
--record(state, {ref, socket, transport}).
+-record(state, {socket, transport}).
 
 -define(AUTH_HEADER, 16).
 -define(SERVER, ?MODULE).
-% Unlimited timeout
--define(TIMEOUT, 0).
+% TODO: Try to find a way to disable timeout
+-define(TIMEOUT, 86400000).
 
 
 
 start_link(Ref, Socket, Transport, Opts) ->
-    gen_server:start_link(?MODULE, [Ref, Socket, Transport, Opts], []).
+    proc_lib:start_link(?MODULE, init, [Ref, Socket, Transport, Opts]).
 
+init([]) -> {ok, undefined}.
 
 stop() ->
-    lager:info("Stopping the listener"),
-    gen_server:cast(?SERVER, stop).
+    lager:info("Stopping the listener").
+%    gen_server:cast(?SERVER, stop).
 
 
-init([Ref, Socket, Transport, _Opts]) ->
-    {ok, #state{ref=Ref, socket=Socket, transport=Transport}, ?TIMEOUT}.
+
+init(Ref, Socket, Transport, _Opts = []) ->
+    ok = proc_lib:init_ack({ok, self()}),
+    ok = ranch:accept_ack(Ref),
+    ok = Transport:setopts(Socket, [{active, once}]),
+    gen_server:enter_loop(?MODULE, [],
+        #state{socket=Socket, transport=Transport},
+        ?TIMEOUT).
 
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
-handle_info(timeout, State) -> auth(State);
+%handle_info(timeout, _State=#state{ref=Ref, socket=Socket, transport=Transport}) ->
+%  ranch:accept_ack(Ref),
+%  Transport:setopts(Socket, [{active, once}]);
+
+  %auth(State);
 
 %% When message from client is received:
-handle_info({tcp, Socket, Data}, State=#state{ref=_Ref, socket=Socket, transport=Transport}) ->
+handle_info({tcp, Socket, Data}, State=#state{socket=Socket, transport=Transport}) ->
     Transport:setopts(Socket, [{active, once}]),
-    Transport:send(Socket, Data),
+    Transport:send(Socket, <<"OK\n">>),
+    {noreply, State, ?TIMEOUT};
 
-%TODO GET CURRENT PID IN REGISTRATION PROCESS AND PUT THEM IN ETC
-%GET ALL PIDS FROM GEN_SERRVER (self())
-%
 
-    %% TODO : return something usefull
-    %%io:format("~tp", [Data]),
-    %%Transport:send(Socket, <<"OK\n">>),
-    {noreply, State};
+% Message broadcasting
+%handle_info({broadcast, Message}, State) ->
+%    lager:info("Broadcasting message ~tp", Message),
+%    {noreply, State};
+
 
 handle_info({tcp_closed, _Socket}, State) ->
     {stop, normal, State};
@@ -69,6 +80,9 @@ handle_info({tcp_error, _, Reason}, State) ->
     {stop, Reason, State};
 
 handle_info(timeout, State) ->
+    {stop, normal, State};
+
+handle_info(_Info, State) ->
     {stop, normal, State}.
 
 
@@ -98,39 +112,39 @@ code_change(_OldVsn, State, _Extra) ->
 %% first 2 bytes unsigned big endian integer length encoded
 %% username in bytes. Following N bytes are the username in
 %% erlang searilization format
-auth(State=#state{ref=Ref, socket=Socket, transport=Transport}) ->
-    ranch:accept_ack(Ref),
+%auth(State=#state{ref=Ref, socket=Socket, transport=Transport}) ->
+%    ranch:accept_ack(Ref),
 
     % Prompt
-    Transport:send(Socket, <<"auth\n">>),
+%    Transport:send(Socket, <<"auth\n">>),
 
     % waiting for username
     %ranch:accept_ack(Ref),
 
-    AuthenticationDelay = 30000, % 30 seconds to authenticate
+%    AuthenticationDelay = 30000, % 30 seconds to authenticate
 
-    case Transport:recv(Socket, 0, AuthenticationDelay) of
-        {ok, Data} ->
-            [Name, Rest] = binary:split(Data, <<"\r\n">>),
-            CurrentProcessId = self(),
-
-            lager:info("User with name ~tp is trying to authenticate", [Name]),
-            lager:info("Checking username ~tp in the database", [Name]),
-            case wall_users:exist(Name) of
-                true ->
-                    lager:info("the following user exist"),
-                    {stop, normal, State};
-                false ->
-                    lager:info("no such user exist. Creating"),
-                    wall_users:reg(Name, CurrentProcessId),
-                    {noreply, State}
-            end;
-
-        {error, _} ->
-            lager:error("bad authentication data... Connection will be dropped"),
-            {stop, normal, State}
-    end.
-
+%    case Transport:recv(Socket, 0, AuthenticationDelay) of
+%        {ok, Data} ->
+%            [Name, Rest] = binary:split(Data, <<"\r\n">>),
+%            CurrentProcessId = self(),
+%
+%            lager:info("User with name ~tp is trying to authenticate", [Name]),
+%            lager:info("Checking username ~tp in the database", [Name]),
+%            case wall_users:exist(Name) of
+%                true ->
+%                    lager:info("the following user exist"),
+%                    {stop, normal, State};
+%                false ->
+%                    lager:info("no such user exist. Creating"),
+%                    wall_users:reg(Name, CurrentProcessId),
+%                    {noreply, State}
+%            end;
+%
+%        {error, _} ->
+%            lager:error("bad authentication data... Connection will be dropped"),
+%            {stop, normal, State}
+%    end.
+%
 
 
 %auth(Ref, Socket, Transport, <<Size:?AUTH_HEADER/unsigned-big-integer, Rest/bits>>) ->
