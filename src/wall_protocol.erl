4a62ca1ad41cbfd728d343376b7f0e0b1189d946
Paul Popoff <oc14@ya.ru>
2016-04-17 17:06:41 +0300

    Handling a case when user is not able to register

--- src/wall_protocol.erl
+++ src/wall_protocol.erl
@@ -21,11 +21,10 @@
 -export([terminate/2]).
 -export([code_change/3]).
 
--record(state, {socket, transport}).
+%-record(state, {auth_status, socket, transport}).
 
 -define(AUTH_HEADER, 16).
 -define(SERVER, ?MODULE).
-% TODO: Try to find a way to disable timeout
 -define(TIMEOUT, 86400000).
 
 
@@ -33,46 +32,90 @@
 start_link(Ref, Socket, Transport, Opts) ->
     proc_lib:start_link(?MODULE, init, [Ref, Socket, Transport, Opts]).
 
+
 init([]) -> {ok, undefined}.
 
-stop() ->
-    lager:info("Stopping the listener").
-%    gen_server:cast(?SERVER, stop).
 
+stop() ->
+    lager:info("Stopping the listener"),
+    gen_server:cast(?SERVER, stop).
 
 
 init(Ref, Socket, Transport, _Opts = []) ->
     ok = proc_lib:init_ack({ok, self()}),
     ok = ranch:accept_ack(Ref),
     ok = Transport:setopts(Socket, [{active, once}]),
-    gen_server:enter_loop(?MODULE, [],
-        #state{socket=Socket, transport=Transport},
-        ?TIMEOUT).
+    gen_server:enter_loop(?MODULE, [], {false, <<>>, Socket, Transport}, ?TIMEOUT).
 
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
-%handle_info(timeout, _State=#state{ref=Ref, socket=Socket, transport=Transport}) ->
-%  ranch:accept_ack(Ref),
-%  Transport:setopts(Socket, [{active, once}]);
 
-  %auth(State);
-
-%% When message from client is received:
-handle_info({tcp, Socket, Data}, State=#state{socket=Socket, transport=Transport}) ->
+%% This code handles authorisation
+handle_info({tcp, Socket, Data}, State={false, _Username, Socket, Transport}) ->
     Transport:setopts(Socket, [{active, once}]),
-    Transport:send(Socket, <<"OK\n">>),
+
+
+    %% About the authentication protocol:
+    %% user sends A message (username) that should be terminated with
+    %% the following sequence '\r\n'
+    %% Then server shoud authenticate the user and return
+    %% 'OK\n'
+    %% The prorocol was made that way because I wan't to be able to test
+    %% using telnet
+    [Name, _Rest] = binary:split(Data, <<"\r\n">>),
+    AuthSucess = <<"OK\n">>,
+
+    lager:info("User with name ~tp is trying to authenticate", [Name]),
+    lager:info("Checking username ~tp in the database", [Name]),
+
+    case wall_users:exist(Name) of
+        true ->
+            lager:info("The specified user exist. Disconnecting..."),
+            {stop, normal, State};
+        false ->
+            lager:info("no such user exist. Creating..."),
+            % Registing the user with current process id
+            wall_users:reg(Name, self()),
+            NewState = {true, Name, Socket, Transport},
+            lager:info("returning new state ~tp", [NewState]),
+            Transport:send(Socket, AuthSucess),
+            {noreply, NewState}
+    end;
+
+
+% SENDING
+% When user is authenticated
+% sends message to all the connected clients
+handle_info({tcp, Socket, Data}, State={true, Username, Socket, Transport}) ->
+    Transport:setopts(Socket, [{active, once}]),
+    notify_other_clients(Username, Data),
     {noreply, State, ?TIMEOUT};
 
 
+% RECEIVING
 % Message broadcasting
-%handle_info({broadcast, Message}, State) ->
-%    lager:info("Broadcasting message ~tp", Message),
-%    {noreply, State};
+handle_info({broadcast, Username, Message}, State={true, _CurrName, Socket, Transport}) ->
+    Transport:setopts(Socket, [{active, once}]),
 
+    % About the protocol:
+    % It should send a number of messages
+    % Header consists of two parts: message length
+    % And name length
+    %PayloadLength = byte_size(Message),
+    %NameLength    = byte_size(Username),
+    %Message = <<PayloadLength:3/unsigned-big-integer,
+    %            NameLength:2/unsigned-big-integer,
+    %            Username/binary,
+    %            Message/binary>>,
 
+    % todo may be add username here
+    Transport:send(Socket, Message),
+    {noreply, State};
+
+% Cases when server will be stopped
 handle_info({tcp_closed, _Socket}, State) ->
     {stop, normal, State};
 
@@ -82,20 +125,33 @@ handle_info({tcp_error, _, Reason}, State) ->
 handle_info(timeout, State) ->
     {stop, normal, State};
 
+handle_info(stop, State) ->
+    {stop, normal, State};
+
 handle_info(_Info, State) ->
     {stop, normal, State}.
-
 
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
-
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
+terminate(_Reason, State={true, Username, _Socket, _Transport}) ->
+    lager:info("Session was terminated"),
+    case wall_users:exist(Username) of
+        true ->  lager:info("removing the user ~tp", [Username]),
+                 %wall_users:del(Username),
+                 ok;
+        false -> lager:info("No user exist. Safe termination"),
+                 ok
+    end;
 
-terminate(_Reason, _State) ->
+
+terminate(_Reason, _State={false, <<>>, _Socket, _Transport}) ->
+    lager:info("Session was terminated, before user logged in."),
     ok.
+
 
 
 code_change(_OldVsn, State, _Extra) ->
@@ -106,62 +162,16 @@ code_change(_OldVsn, State, _Extra) ->
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
 
+notify_other_clients(Username, Message) ->
+    broadcast_message(
+        wall_users:active_connections_except(self()),
+        Username, Message
+    ).
 
 
-%% authentication protocol
-%% first 2 bytes unsigned big endian integer length encoded
-%% username in bytes. Following N bytes are the username in
-%% erlang searilization format
-%auth(State=#state{ref=Ref, socket=Socket, transport=Transport}) ->
-%    ranch:accept_ack(Ref),
-
-    % Prompt
-%    Transport:send(Socket, <<"auth\n">>),
-
-    % waiting for username
-    %ranch:accept_ack(Ref),
-
-%    AuthenticationDelay = 30000, % 30 seconds to authenticate
-
-%    case Transport:recv(Socket, 0, AuthenticationDelay) of
-%        {ok, Data} ->
-%            [Name, Rest] = binary:split(Data, <<"\r\n">>),
-%            CurrentProcessId = self(),
-%
-%            lager:info("User with name ~tp is trying to authenticate", [Name]),
-%            lager:info("Checking username ~tp in the database", [Name]),
-%            case wall_users:exist(Name) of
-%                true ->
-%                    lager:info("the following user exist"),
-%                    {stop, normal, State};
-%                false ->
-%                    lager:info("no such user exist. Creating"),
-%                    wall_users:reg(Name, CurrentProcessId),
-%                    {noreply, State}
-%            end;
-%
-%        {error, _} ->
-%            lager:error("bad authentication data... Connection will be dropped"),
-%            {stop, normal, State}
-%    end.
-%
-
-
-%auth(Ref, Socket, Transport, <<Size:?AUTH_HEADER/unsigned-big-integer, Rest/bits>>) ->
-%    <<UserNameBin:Size/binary>> = Rest,
-%    UserName = binary_to_term(UserNameBin),
-%    lager:info("Auth request from user: ~tp", [UserName]),
-%    case wall_users:exist(UserName) of
-%        true  ->
-%            lager:info("User doesnt exist... Registration"),
-            %wall_users:reg(UserName),
-%            lager:info("authenticated ~tp", [UserName]),
-%            Transport:send(Socket, <<"granted">>),
-%            gen_server:enter_loop(
-%                ?MODULE, [], #state{ref=Ref, socket=Socket, transport=Transport}, ?TIMEOUT);
-%        false ->
-%            lager:info("authentication failed"),
-%            error
-%    end.
-
+broadcast_message([], _Username, _Message) ->
+    ok;
+broadcast_message([Pid|Pids], Username, Message) ->
+    Pid ! {broadcast, Username, Message},
+    broadcast_message(Pids, Username, Message).
 
